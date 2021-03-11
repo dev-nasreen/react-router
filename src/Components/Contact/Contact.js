5dc30a38bc78cb68c7c279a1acb2148f8fa4de68
dev-nasreen <nasreen.akter34@gmail.com>
2021-03-11 22:47:19 +0600

    Contact page added

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,10 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from '../Header/Header';
 
 const Contact = () => {
+    const [data, setData] = useState({
+        fullName:'',
+        phone:'',
+        email: '',
+        sms:''
+    })
+
+    const InputEvent = (event) => {
+        const {name, value} = event.target;
+
+        setData((preVal) =>{
+            return {
+                ...preVal, [name] : value
+            }
+        })
+    }
+    const formSubmit = (e) => {
+        e.preventDefault();
+
+        alert(`My name is ${data.fullName}. My mobile no. is ${data.phone}. My email is ${data.email}. My msg is ${data.sms}.`);
+    };
     return (
-        <div>
-            <h2>This is contact page.</h2>
-        </div>
+        <>
+            <Header></Header>
+            <div className="my-5">
+                <h1 className="text-center">Contact Us</h1>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 col-10 mx-auto">
+                        <form onSubmit={formSubmit}>
+                            <div class="mb-3">
+                                <label for="exampleFormControlInput1" class="form-label">Full Name</label>
+                                <input type="text" class="form-control" id="exampleFormControlInput1" name="fullName" value={data.fullName} onChange={InputEvent}   placeholder="Enter your name" />
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleFormControlInput1" class="form-label">Phone</label>
+                                <input type="number" class="form-control" id="exampleFormControlInput1" name="phone" value={data.phone} onChange={InputEvent}   placeholder="Mobile number" />
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                                <input type="email" class="form-control" id="exampleFormControlInput1" name="email" value={data.email} onChange={InputEvent}   placeholder="name@example.com" />
+                            </div>
+                                <div class="mb-3">
+                                    <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="sms" value={data.sms} onChange={InputEvent}></textarea>
+                                </div>
+                                <div class="col-12">
+                                    <button class="btn btn-primary" type="submit">Submit form</button>
+                                </div>
+                        </form>
+                    </div>
+                    </div>
+
+                </div>
+        </>
     );
 };
 
